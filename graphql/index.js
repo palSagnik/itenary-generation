0a11d32ce5b9f36bedcd2c0b0bbfb2b611ec8821
SAGNIK PAL <palsagnik2102@gmail.com>
2025-01-01 23:55:37 +0530

    [Chore] Move backend setup to /backend
    Signed-off-by: SAGNIK PAL <palsagnik2102@gmail.com>

--- graphql/index.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "@apollo/server"
-import { env } from "../config/environment"
-
-const apolloServer = new ApolloServer({
-    
-    // Schema Pending
-    playground: env.development
-})
-export default apolloServer

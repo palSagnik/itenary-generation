cdd1a9af6f68a0a7efdbe30ef2087e6bbfa126be
SAGNIK PAL <palsagnik2102@gmail.com>
2025-01-07 01:41:21 +0530

    [Feature] Add Header component
    Signed-off-by: SAGNIK PAL <palsagnik2102@gmail.com>

---
+++ frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div style={{height: "800px", background: "black"}}>Banner</div>
+  )
+}
+
+export default Banner
---
+++ frontend/src/components/common/header/Header.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react' // Add useState
+import { Container, Row, Navbar, Nav, Offcanvas, NavDropdown } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import "../header/Header.css"
+
+const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setOpen(!open)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky)
+    return ()=>{
+      window.removeEventListener("scroll", isSticky)
+    }
+  })
+
+  // sticky header
+  const isSticky = (e) => {
+    const header = document.querySelector('.header-section')
+    const scrollTop = window.scrollY
+    scrollTop >= 120 ? header.classList.add('is-sticky') :
+    header.classList.remove('is-sticky')
+  }
+
+  return (
+    <section className='header-section'>
+      <Container>
+        <Row>
+          <Navbar expand="lg" className="bg-body-tertiary mb-3"> {/* Fixed typo in expand */}
+            <Navbar.Brand>
+              <NavLink to='/' className="text-decoration-none"> Wanderlust </NavLink>
+            </Navbar.Brand>
+
+            <Navbar.Toggle 
+              aria-controls={`offcanvasNavbar-expand-lg`} 
+              onClick={toggleMenu}
+            >
+              <i className={open ? "bi bi-x-lg" : "bi bi-list"}></i>
+            </Navbar.Toggle>
+
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="start"
+              show={open}
+              onHide={() => setOpen(false)}
+            >
+              <Offcanvas.Header closeButton>
+                <h1 className='logo'>Wanderlust</h1>
+              </Offcanvas.Header>
+
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                  <Nav.Link as={NavLink} to="/about">About Us</Nav.Link>
+                  <NavDropdown
+                    title="Itinerary" 
+                    id={`offcanvasNavbarDropdown-expand-lg`}
+                  >
+                    <NavDropdown.Item as={NavLink} to="/flights">Flights</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/hotels">Hotels</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/poi">Points Of Interest</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/weather">Weather Forecast</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+
+            <div className='ms-md-4 ms-2'>
+              <NavLink to="/book" className='btn btn-primary d-none d-sm-inline-block text-decoration-none'>
+                Book Now
+              </NavLink>
+            </div>
+          </Navbar>
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default Header
